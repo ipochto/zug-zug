757122a5c57e6a46af0cf657fb53cdaedd50d204
alyokhin <ipochto@gmail.com>
2025-08-10 22:31:05 +0300

    1. Fixed doctest cmake module discover 2. Creating tests when testing is enabled.

--- cmake/Tests.cmake
+++ cmake/Tests.cmake
@@ -1,22 +1,29 @@
 include(CTest)
 
-find_package(doctest CONFIG QUIET)
-if (NOT doctest_FOUND)
-    message(STATUS "doctest not found, fetching with FetchContent...")
-    FetchContent_Declare(
-        doctest
-        GIT_REPOSITORY https://github.com/doctest/doctest.git 
-        GIT_TAG        v2.4.12
+if(BUILD_TESTING)
+    find_package(doctest CONFIG QUIET)
+    if (NOT doctest_FOUND)
+        message(STATUS "doctest not found, fetching with FetchContent...")
+        FetchContent_Declare(
+            doctest
+            GIT_REPOSITORY https://github.com/doctest/doctest.git 
+            GIT_TAG        v2.4.12
+        )
+        FetchContent_MakeAvailable(doctest)
+        set(CMAKE_MODULE_PATH
+            ${CMAKE_MODULE_PATH} 
+            "${doctest_SOURCE_DIR}/scripts/cmake"
+        )
+    endif()
+
+    include(doctest)
+
+    add_executable(tests 
+        tests/main.cpp
+        tests/dummy.cpp
     )
-    FetchContent_MakeAvailable(doctest)
-    include(${doctest_SOURCE_DIR}/scripts/cmake/doctest.cmake)
+    target_compile_features(tests PRIVATE cxx_std_20)
+    target_link_libraries(tests PRIVATE doctest::doctest)
+
+    doctest_discover_tests(tests)
 endif()
-
-add_executable(tests 
-    tests/main.cpp
-    tests/dummy.cpp
-)
-target_compile_features(tests PRIVATE cxx_std_20)
-target_link_libraries(tests PRIVATE doctest::doctest)
-
-doctest_discover_tests(tests)
