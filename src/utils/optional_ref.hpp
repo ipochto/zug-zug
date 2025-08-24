0931e1119aa5cca89783584186f2af314b9893fb
alyokhin <ipochto@gmail.com>
2025-08-24 20:09:18 +0300

    Make available std::optional<const T&>

---
+++ src/utils/optional_ref.hpp
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <memory>
+#include <optional>
+#include <functional>
+
+template <typename T>
+class optional_ref
+{
+public:
+	optional_ref() = default;
+	optional_ref(std::nullopt_t) : ref(std::nullopt) {}
+	optional_ref(T& value) : ref(value) {}
+
+	explicit operator bool() const noexcept { return has_value(); }
+
+	T& operator*() noexcept { return ref->get(); }
+	const T& operator*() const noexcept { return ref->get(); }
+
+	T* operator->() noexcept { return std::addressof(**this); }
+	const T* operator->() const noexcept { return std::addressof(**this); }
+
+	bool operator==(std::nullopt_t) const noexcept { return !has_value(); }
+	bool operator!=(std::nullopt_t) const noexcept { return has_value(); }
+
+	bool has_value() const noexcept { return ref.has_value(); }
+	
+	void reset() noexcept { ref.reset(); }
+
+private:    
+	std::optional<std::reference_wrapper<T>> ref;
+};
+
+template <typename T>
+using opt_ref = optional_ref<T>;
+
+template <typename T>
+using opt_cref = optional_ref<const T>;
