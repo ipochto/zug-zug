cf676958dc2cdb7881097b81925e54f7db96d895
alyokhin <ipochto@gmail.com>
2025-09-29 15:04:18 +0300

    fix typo

--- src/scripts/lua/sol2.hpp
+++ src/scripts/lua/sol2.hpp
@@ -1,9 +1,9 @@
 #pragma once
 
-// sol2 configuraion section:
+// sol2 configuration section:
 #define SOL_ALL_SAFETIES_ON 1
 #define SOL_LUA_VERSION 501
 #define SOL_ENABLE_INTEROP 1 // MUST be defined to use interop features
-// end of configuraion section
+// end of configuration section
 
 #include <sol/sol.hpp>
