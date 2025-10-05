8ecf4f88dbfb6061cbb1a3f320618245790ca713
alyokhin <ipochto@gmail.com>
2025-10-05 16:29:10 +0300

    Fixed path comparison for roots with trailing separator cases

--- src/utils/filesystem.hpp
+++ src/utils/filesystem.hpp
@@ -7,10 +7,26 @@
 
 namespace fs = std::filesystem;
 
+template <typename T>
+concept fsPaths =
+	std::ranges::range<T>
+	&& std::same_as<std::remove_cvref_t<std::ranges::range_value_t<T>>, fs::path>;
+
 /*----------------------------------------------------------------------------
 --  Utils
 ----------------------------------------------------------------------------*/
 namespace fs_utils {
+
+	[[nodiscard]]
+	inline auto normalize(const fs::path &path)
+		-> fs::path
+	{
+		auto result = path.lexically_normal();
+		if (result.native().ends_with(fs::path::preferred_separator)) {
+			return result.parent_path();
+		}
+		return result;
+	}	
 
 	[[nodiscard]]
 	inline bool startsWith(const fs::path &path, const fs::path &root)
@@ -18,17 +34,14 @@ namespace fs_utils {
 		if (root.empty()) {
 			return false;
 		}
-		const auto rootNorm = fs::absolute(root).lexically_normal();
-		const auto pathNorm = fs::absolute(path).lexically_normal();
-
+		const auto rootNorm = normalize(fs::absolute(root));
+		const auto pathNorm = normalize(fs::absolute(path));
 		const auto [rootEnd, _] = std::ranges::mismatch(rootNorm, pathNorm);
 		return rootEnd == rootNorm.end();
 	}
 
-	template <std::ranges::input_range Range>
-	requires std::same_as<std::ranges::range_value_t<Range>, fs::path>
 	[[nodiscard]]
-	inline bool startsWith(const fs::path &path, const Range &roots)
+	inline bool startsWith(const fs::path &path, const fsPaths auto &roots)
 	{
 		if (roots.empty()) {
 			return false;
